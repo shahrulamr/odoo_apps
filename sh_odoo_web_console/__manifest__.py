895734cdab7aa8995138931323a3425403bb0dab
shahrulamr <shahrulmpv@gmail.com>
2022-09-26 07:41:55 +0000

    First commit

---
+++ sh_odoo_web_console/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Odoo 14 Web Console',
+    'version': '14.0.1.0.0',
+    'category': 'Technical',
+    'author': 'ShahrulAmr',
+    'maintainer': 'ShahrulAmr',
+    'website': 'https://www.linkedin.com/in/shahrul-amirudin',
+    'summary': 'Odoo Web Console',
+    'description': 'Run and compile python with Odoo environments',
+    'license': 'LGPL-3',
+    'depends': ['base'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/assets.xml',
+        'views/sh_odoo_web_console_views.xml',
+    ],
+    'demo': [],
+    'qweb': [],
+    'application': True,
+    'installable': True,
+    'auto_install': False
+}
